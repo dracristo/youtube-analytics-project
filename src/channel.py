8d32099afcdd28ef73771e3e6eae509fb8dc6906
dracristo <coding.enclosure@gmail.com>
2024-01-30 21:43:47 +0500

    сделал дз #1

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,17 @@
+from googleapiclient.discovery import build
+import os
+import json
 
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        """Экземпляр инициализирует id канала. Дальше все данные будут подтягиваться по API."""
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        youtube = build('youtube', 'v3', developerKey=os.getenv("API"))
+        stats = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(stats, indent=2, ensure_ascii=False))
